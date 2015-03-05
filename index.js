e53fb43074384506711c60a65ce72ad65178da06
Zachary Cava <zachary.cava@hulu.com>
2015-03-05 01:07:38 -0800

    Initial version, basic wrapping and bare shuttle option

---
+++ index.js
@@ -0,0 +1,40 @@
+var through  = require('through2')
+var coverage = require('coffee-coverage')
+var gutil    = require('gulp-util')
+
+var PLUGIN_NAME = "gulp-coffee-coverage"
+
+module.exports = function(options) {
+    if(!options) {
+        options = {};
+    }
+
+    var instrumenter = new coverage.CoverageInstrumentor();
+
+    // file processing function
+    var coverFile = function(file, enc, cb) {
+        if(file.isNull()) {
+            return cb(null, file);
+        }
+
+        if(file.isStream()) {
+            return cb(new gutil.PluginError(PLUGIN_NAME, "Streams are not supported"));
+        }
+
+        if(file.isBuffer()) {
+            var fileContents = file.contents.toString(enc);
+            var fileName = file.path.charAt(0) + file.path;
+            var covered = instrumenter.instrumentCoffee(fileName, fileContents, {
+                bare : options.bare,
+                path : 'relative'
+            });
+            file.contents = new Buffer(covered.init + covered.js, enc);
+            file.path = gutil.replaceExtension(file.path, '.js');
+        }
+
+        return cb(null, file);
+    }
+
+    // create handle for processing
+    return through.obj(coverFile)
+}
